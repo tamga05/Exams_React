af2f8d065e8096bc0094d881de6639fbd027e149
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-07-08 00:56:17 +0300

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -7,13 +7,324 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
+
     <App />
+
   </React.StrictMode>
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+
+// //==============================================================================================================================================================
+// // Экзамены Понедельник 1-неделя.
+// //==============================================================================================================================================================
+//
+// //1.
+//
+// export const Wrapper  = () => {
+//     return <user city="minsk" />
+// }
+//
+// type PropsType = {
+//     city: string
+// }
+//
+// export const user: React.FC<PropsType> = (props) => {
+//     return <div>hello</div>
+// }
+//
+// //2.
+//
+// import ReactDOM from 'react-dom'
+//
+// export const YoutubeVideo = () => {
+//     return <div>
+//         <VideoHeader />
+//         <VideoContent />
+//         <VideoStatistics />
+//     </div>
+// }
+// export const VideoHeader = () => {
+//     return <div>
+//         😀 Заголовок видео
+//     </div>
+// }
+// export const VideoContent = () => {
+//     return <div>
+//         📼 Контент видео
+//     </div>
+// }
+// export const VideoStatistics = () => {
+//     return <div>
+//         📊 Статистика лайков
+//     </div>
+// }
+//
+// ReactDOM.render(<div>ххх</div>,
+//     document.getElementById('root')
+// );
+//
+// //Что нужно написать вместо ххх, чтобы на экране увидеть:
+// //😀 Заголовок видео
+// //📼 Контент видео
+// //📊 Статистика лайков
+// // ❗ Ответ дать минимально возможным объёмом кода
+//
+// //3.
+//
+// type UserPropsType = {
+//     name: string
+//     description: string
+// }
+// export const User: React.FC<UserPropsType> = (props) => {
+//     return <div>
+//         <h1>Имя: {props.name}</h1>
+//         //<div>Описание: {ххх}</div>
+//     </div>
+// }
+//
+// //Что нужно написать вместо ххх, что бы код работал?
+//
+// //4.
+//
+// type PropsType = {
+//     city: string
+//     country: string
+//     coords?: string
+// }
+// // 'belarus', 'minsk', '53.917501,27.604851'
+// export const Wrapper1 = () => {
+//     return <PropsComponent1 city='minsk'/>
+// }
+//
+// export const PropsComponent1: React.FC<PropsType> = (props) => {
+//     return <div>hello</div>
+// }
+//
+// //5.
+//
+// type PagePropsType = {
+//     age: number
+//     name: string
+//     avatarUrl: string
+// }
+// const Page: React.FC<PagePropsType> = (props) => {
+//     return <User a={xxx} n={yyy} />
+// }
+// type UserPropsType = {
+//     a: number
+//     n: string
+// }
+// export const User: React.FC<UserPropsType> = (props) => {
+//     return <div>name: {props.n}, age: {props.a}</div>
+// }
+//
+// //6.
+//
+// type NewsType = {
+//     title: string
+//     author: string
+// }
+// type ArticleType = {
+//     title: string
+//     date: string
+//     text: string
+// }
+// type PagePropsType = {
+//     news: NewsType[]
+//     mainArticle: ArticleType
+// }
+// export const Page: React.FC<PagePropsType> = (props) => {
+//     return <div>
+//         <article>
+//             <h1>Название: {props.XXX.title}</h1>
+//             <div>{props.XXX.date}</div>
+//             <div>{props.XXX.text}</div>
+//         </article>
+//         <aside>Articles:
+//             <ul>
+//                 {props.YYY.map(n => <li>{n.title}, {n.author}</li>)}
+//             </ul>
+//         </aside>
+//     </div>
+// }
+//
+// //7.
+//
+// type UserWalletType = {
+//     title: string
+//     amount: number
+// }
+// type UserWalletPropsType = {
+//     wallet: UserWalletType
+// }
+//
+// export const UserWallet: React.FC<UserWalletPropsType> = (props) => {
+//     return <div>title: {props.wallet.title}, amount: {props.wallet.amount}</div>
+// }
+//
+// export const UserMoney = () => {
+//     const wallets = [
+//         {title: 'bitcoin', amount: 1},
+//         {title: '$', amount: 100}
+//     ]
+//
+//     return <div>
+//         <UserWallet wallet={xxx} />
+//         <UserWallet wallet={yyy} />
+//     </div>
+// }
+//
+// //8.
+//
+// import ReactDOM from 'react-dom'
+//
+// export const VideoHeader = (props: {videoName: string}) => {
+//     return <div>
+//         😀 {props.videoName}
+//     </div>
+// }
+// export const VideoContent = (props: {videoContent: string}) => {
+//     return <div>
+//         📼 <a href={props.videoContent}>{props.videoContent}</a>
+//     </div>
+// }
+// export const VideoDescription = (props: {videoDescription: string}) => {
+//     return <div>
+//         📑 {props.videoDescription}
+//     </div>
+// }
+//
+// export const YoutubeVideo = (props: any) => {
+//     return <div>
+//         <VideoHeader xxx />
+//         <VideoContent yyy />
+//         <VideoDescription zzz />
+//     </div>
+// }
+//
+// export const App = () => {
+//     const video = {
+//         title: 'Samurai way',
+//         link: 'https://www.youtube.com/watch?v=gb7gMluAeao&list=PLcvhF2Wqh7DNVy1OCUpG3i5lyxyBWhGZ8',
+//         description: 'Best free react-course'
+//     }
+//
+//     return <YoutubeVideo video={video} />
+// }
+//
+// ReactDOM.render(<App />,
+//     document.getElementById('root')
+// );
+//
+// //9.
+//
+// import ReactDOM from 'react-dom'
+//
+// const CrazyButton = (props: any) => {
+//
+//     const style = {
+//         color: props.XXX,
+//         backgroundColor: props.YYY
+//     }
+//
+//     return <button style={style}>
+//         {props.ZZZ}
+//     </button>
+// }
+//
+// export const App = () => {
+//     return <div>
+//         <CrazyButton title={'delete'} fontColor={'white'} bgColor={'red'}/>
+//         <CrazyButton title={'add'} fontColor={'white'} bgColor={'green'}/>
+//     </div>
+// }
+//
+// ReactDOM.render(<App/>,
+//     document.getElementById('root')
+// )
+//
+// //10.
+//
+// import ReactDOM from 'react-dom'
+//
+// const Son = (props: any) => {
+//     return <div>
+//         I am son. My name is {props.name}
+//     </div>
+// }
+//
+//
+// const Father = (props: any) => {
+//     return <div>
+//         I am father. My name is {props.name}
+//         <Son name={props.sonName} />
+//     </div>
+// }
+//
+// const Granny = (props: any) => {
+//     return <div>
+//         I am granny. My name is {props.name}
+//         <Father name={props.fatherName} sonName={props.sonName} />
+//     </div>
+// }
+//
+// export const App = () => {
+//     return <div>
+//         <Granny XXX={'Бабуля'} YYY={'Батя'} ZZZ={'Сын'}/>
+//     </div>
+// }
+//
+// ReactDOM.render(<App/>,
+//     document.getElementById('root')
+// )
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
