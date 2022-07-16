90572a3d1d72b33e8521ae617e7264a52e60c422
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-07-17 00:07:24 +0300

    task solving 10

--- src/components/Monday_1.tsx
+++ src/components/Monday_1.tsx
@@ -232,64 +232,64 @@ export default Monday_1;
 
 //9.
 
-import ReactDOM from 'react-dom'
-
-const CrazyButton = (props: any) => {
-
-  const style = {
-    color: props.fontColor,
-    backgroundColor: props.bgColor
-  }
-
-  return <button style={style}>
-    {props.title}
-  </button>
-}
-
-export const App = () => {
-  return <div>
-    <CrazyButton title={'delete'} fontColor={'white'} bgColor={'red'}/>
-    <CrazyButton title={'add'} fontColor={'white'} bgColor={'green'}/>
-  </div>
-}
-
-ReactDOM.render(<App/>,
-    document.getElementById('root')
-)
-
-//10.
-
 // import ReactDOM from 'react-dom'
 //
-// const Son = (props: any) => {
-//   return <div>
-//     I am son. My name is {props.name}
-//   </div>
-// }
+// const CrazyButton = (props: any) => {
 //
+//   const style = {
+//     color: props.fontColor,
+//     backgroundColor: props.bgColor
+//   }
 //
-// const Father = (props: any) => {
-//   return <div>
-//     I am father. My name is {props.name}
-//     <Son name={props.sonName} />
-//   </div>
-// }
-//
-// const Granny = (props: any) => {
-//   return <div>
-//     I am granny. My name is {props.name}
-//     <Father name={props.fatherName} sonName={props.sonName} />
-//   </div>
+//   return <button style={style}>
+//     {props.title}
+//   </button>
 // }
 //
 // export const App = () => {
 //   return <div>
-//     <Granny name={'Бабуля'} fatherName={'Батя'} sonName={'Сын'}/>
+//     <CrazyButton title={'delete'} fontColor={'white'} bgColor={'red'}/>
+//     <CrazyButton title={'add'} fontColor={'white'} bgColor={'green'}/>
 //   </div>
 // }
 //
 // ReactDOM.render(<App/>,
 //     document.getElementById('root')
 // )
+
+//10.
+
+import ReactDOM from 'react-dom'
+
+const Son = (props: any) => {
+  return <div>
+    I am son. My name is {props.name}
+  </div>
+}
+
+
+const Father = (props: any) => {
+  return <div>
+    I am father. My name is {props.name}
+    <Son name={props.sonName} />
+  </div>
+}
+
+const Granny = (props: any) => {
+  return <div>
+    I am granny. My name is {props.name}
+    <Father name={props.fatherName} sonName={props.sonName} />
+  </div>
+}
+
+export const App = () => {
+  return <div>
+    <Granny name={'Бабуля'} fatherName={'Батя'} sonName={'Сын'}/>
+  </div>
+}
+
+ReactDOM.render(<App/>,
+    document.getElementById('root')
+)
 
 
