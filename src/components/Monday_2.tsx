2fb2cc849a4a72105db28ba478d214d452e9ab88
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-07-20 00:06:15 +0300

    task solving Mon-2-5

--- src/components/Monday_2.tsx
+++ src/components/Monday_2.tsx
@@ -81,32 +81,13 @@ export default Monday_2;
 
 //4.
 
-import React, {useState} from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-
-function UsersList() {
-    const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
-
-    return (
-        <p>Тут будет список пользователей</p>
-    )
-}
-
-ReactDOM.render(
-    <UsersList/>, document.getElementById('root')
-);
-
-// Что вернёт выражение: Array.isArray(users)?
-
-//5.
-
 // import React, {useState} from 'react';
 // import ReactDOM from 'react-dom';
 // import './index.css';
 //
 // function UsersList() {
 //     const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
+//
 //     return (
 //         <p>Тут будет список пользователей</p>
 //     )
@@ -115,6 +96,25 @@ ReactDOM.render(
 // ReactDOM.render(
 //     <UsersList/>, document.getElementById('root')
 // );
+
+// Что вернёт выражение: Array.isArray(users)?
+
+//5.
+
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function UsersList() {
+    const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
+    return (
+        <p>Тут будет список пользователей</p>
+    )
+}
+
+ReactDOM.render(
+    <UsersList/>, document.getElementById('root')
+);
 
 // Что вернёт выражение: typeof setUsers?
 
