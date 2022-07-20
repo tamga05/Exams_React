f2cf1961a483f43391f599eca65efc351496098f
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-07-21 00:12:25 +0300

    task solving Mon-2-6

--- src/components/Monday_2.tsx
+++ src/components/Monday_2.tsx
@@ -101,45 +101,45 @@ export default Monday_2;
 
 //5.
 
-import React, {useState} from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-
-function UsersList() {
-    const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
-    return (
-        <p>Тут будет список пользователей</p>
-    )
-}
-
-ReactDOM.render(
-    <UsersList/>, document.getElementById('root')
-);
-
-// Что вернёт выражение: typeof setUsers?
-
-//6.
-
 // import React, {useState} from 'react';
 // import ReactDOM from 'react-dom';
 // import './index.css';
 //
 // function UsersList() {
 //     const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
-//
 //     return (
-//         <main>
-//             <h4>User list:</h4>
-//             <ul>
-//                 { users.ХХХ(user => <li>{user}</li> )}
-//             </ul>
-//         </main>
+//         <p>Тут будет список пользователей</p>
 //     )
 // }
 //
 // ReactDOM.render(
 //     <UsersList/>, document.getElementById('root')
 // );
+
+// Что вернёт выражение: typeof setUsers?
+
+//6.
+
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function UsersList() {
+    const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
+
+    return (
+        <main>
+            <h4>User list:</h4>
+            <ul>
+                { users.map(user => <li>{user}</li> )}
+            </ul>
+        </main>
+    )
+}
+
+ReactDOM.render(
+    <UsersList/>, document.getElementById('root')
+);
 
 // Что надо вставить вместо ХХХ, чтобы код заработал?
 
