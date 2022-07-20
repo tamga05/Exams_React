99c7fa62cc68d7fc714006a129adf01c7dfdfa1a
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-07-21 00:14:35 +0300

    task solving Mon-2-7

--- src/components/Monday_2.tsx
+++ src/components/Monday_2.tsx
@@ -120,45 +120,18 @@ export default Monday_2;
 
 //6.
 
-import React, {useState} from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-
-function UsersList() {
-    const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
-
-    return (
-        <main>
-            <h4>User list:</h4>
-            <ul>
-                { users.map(user => <li>{user}</li> )}
-            </ul>
-        </main>
-    )
-}
-
-ReactDOM.render(
-    <UsersList/>, document.getElementById('root')
-);
-
-// Что надо вставить вместо ХХХ, чтобы код заработал?
-
-//7.
-
 // import React, {useState} from 'react';
 // import ReactDOM from 'react-dom';
 // import './index.css';
 //
 // function UsersList() {
 //     const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
-//     const getUser = (user: string) => <li>{user}</li>
-//     const usersList = users.map(XXX)
 //
 //     return (
 //         <main>
 //             <h4>User list:</h4>
 //             <ul>
-//                 {usersList}
+//                 { users.map(user => <li>{user}</li> )}
 //             </ul>
 //         </main>
 //     )
@@ -167,6 +140,33 @@ ReactDOM.render(
 // ReactDOM.render(
 //     <UsersList/>, document.getElementById('root')
 // );
+
+// Что надо вставить вместо ХХХ, чтобы код заработал?
+
+//7.
+
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function UsersList() {
+    const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
+    const getUser = (user: string) => <li>{user}</li>
+    const usersList = users.map(getUser)
+
+    return (
+        <main>
+            <h4>User list:</h4>
+            <ul>
+                {usersList}
+            </ul>
+        </main>
+    )
+}
+
+ReactDOM.render(
+    <UsersList/>, document.getElementById('root')
+);
 
 // Что надо вставить вместо XXX, чтобы код заработал?
 
