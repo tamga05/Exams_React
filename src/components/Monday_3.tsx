73f702237d4a462da103f1991392a3f762a90761
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-07-24 00:03:14 +0300

    task solving Mon-3-2

--- src/components/Monday_3.tsx
+++ src/components/Monday_3.tsx
@@ -16,55 +16,6 @@ export default Monday_3;
 
 //1.
 
-import React, {useState} from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-
-type UserType = {
-    id: number
-    name: string
-    age: number
-}
-
-function User(props: UserType) {
-    return (
-        <li>Student {props.name}: {props.age} y.o.</li>
-    )
-}
-
-function UsersList() {
-    const data: Array<UserType> = [
-        {id: 1, name: "Bob", age: 34},
-        {id: 2, name: "Alex", age: 29},
-        {id: 3, name: "Ann", age: 25},
-        {id: 4, name: "John", age: 36},
-    ]
-    const [users, setUsers] = useState<Array<UserType>>(data)
-    return (
-        <main>
-            <h4>User list:</h4>
-            <ul>
-                {
-                    users.map(u => <User key={u.id}                          // key
-                                         id={u.id}
-                                         name={u.name}
-                                         age={u.age}
-                        />
-                    )
-                }
-            </ul>
-        </main>
-    )
-}
-
-ReactDOM.render(
-    <UsersList/>, document.getElementById('root')
-);
-
-// Что надо написать вместо xxx, чтобы код работал оптимально?
-
-//2.
-
 // import React, {useState} from 'react';
 // import ReactDOM from 'react-dom';
 // import './index.css';
@@ -82,21 +33,25 @@ ReactDOM.render(
 // }
 //
 // function UsersList() {
-//
 //     const data: Array<UserType> = [
 //         {id: 1, name: "Bob", age: 34},
 //         {id: 2, name: "Alex", age: 29},
 //         {id: 3, name: "Ann", age: 25},
 //         {id: 4, name: "John", age: 36},
 //     ]
-//
 //     const [users, setUsers] = useState<Array<UserType>>(data)
-//
 //     return (
 //         <main>
 //             <h4>User list:</h4>
 //             <ul>
-//                 { users.map(u => <User key={u.id} {...xxx} />) }                  // u
+//                 {
+//                     users.map(u => <User key={u.id}                          // key
+//                                          id={u.id}
+//                                          name={u.name}
+//                                          age={u.age}
+//                         />
+//                     )
+//                 }
 //             </ul>
 //         </main>
 //     )
@@ -105,6 +60,51 @@ ReactDOM.render(
 // ReactDOM.render(
 //     <UsersList/>, document.getElementById('root')
 // );
+
+// Что надо написать вместо xxx, чтобы код работал оптимально?
+
+//2.
+
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+type UserType = {
+    id: number
+    name: string
+    age: number
+}
+
+function User(props: UserType) {
+    return (
+        <li>Student {props.name}: {props.age} y.o.</li>
+    )
+}
+
+function UsersList() {
+
+    const data: Array<UserType> = [
+        {id: 1, name: "Bob", age: 34},
+        {id: 2, name: "Alex", age: 29},
+        {id: 3, name: "Ann", age: 25},
+        {id: 4, name: "John", age: 36},
+    ]
+
+    const [users, setUsers] = useState<Array<UserType>>(data)
+
+    return (
+        <main>
+            <h4>User list:</h4>
+            <ul>
+                { users.map(u => <User key={u.id} {...u} />) }                  // u
+            </ul>
+        </main>
+    )
+}
+
+ReactDOM.render(
+    <UsersList/>, document.getElementById('root')
+);
 
 // Что надо написать вместо xxx, чтобы код работал?
 
