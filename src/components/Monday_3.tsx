75b6d7bfa61797cded637f6e9bd115d2f7ee4b99
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-07-24 00:04:17 +0300

    task solving Mon-3-3

--- src/components/Monday_3.tsx
+++ src/components/Monday_3.tsx
@@ -65,51 +65,6 @@ export default Monday_3;
 
 //2.
 
-import React, {useState} from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-
-type UserType = {
-    id: number
-    name: string
-    age: number
-}
-
-function User(props: UserType) {
-    return (
-        <li>Student {props.name}: {props.age} y.o.</li>
-    )
-}
-
-function UsersList() {
-
-    const data: Array<UserType> = [
-        {id: 1, name: "Bob", age: 34},
-        {id: 2, name: "Alex", age: 29},
-        {id: 3, name: "Ann", age: 25},
-        {id: 4, name: "John", age: 36},
-    ]
-
-    const [users, setUsers] = useState<Array<UserType>>(data)
-
-    return (
-        <main>
-            <h4>User list:</h4>
-            <ul>
-                { users.map(u => <User key={u.id} {...u} />) }                  // u
-            </ul>
-        </main>
-    )
-}
-
-ReactDOM.render(
-    <UsersList/>, document.getElementById('root')
-);
-
-// Что надо написать вместо xxx, чтобы код работал?
-
-//3.
-
 // import React, {useState} from 'react';
 // import ReactDOM from 'react-dom';
 // import './index.css';
@@ -122,25 +77,26 @@ ReactDOM.render(
 //
 // function User(props: UserType) {
 //     return (
-//         <li>User {props.name}: {props.age} y.o.</li>
+//         <li>Student {props.name}: {props.age} y.o.</li>
 //     )
 // }
 //
 // function UsersList() {
+//
 //     const data: Array<UserType> = [
-//         {id: 1, name: "Bob", age: 25},
-//         {id: 2, name: "Alex", age: 28},
-//         {id: 3, name: "Ann", age: 23},
-//         {id: 4, name: "John", age: 30},
+//         {id: 1, name: "Bob", age: 34},
+//         {id: 2, name: "Alex", age: 29},
+//         {id: 3, name: "Ann", age: 25},
+//         {id: 4, name: "John", age: 36},
 //     ]
+//
 //     const [users, setUsers] = useState<Array<UserType>>(data)
-//     // Пользователи старше 25 лет:
-//     const olderThen25Users = users.xxx(u => u.age > 25)                           // filter
+//
 //     return (
 //         <main>
 //             <h4>User list:</h4>
 //             <ul>
-//                 { olderThen25Users.map(u => <User key={u.id} {...u}/>)}
+//                 { users.map(u => <User key={u.id} {...u} />) }                  // u
 //             </ul>
 //         </main>
 //     )
@@ -149,6 +105,50 @@ ReactDOM.render(
 // ReactDOM.render(
 //     <UsersList/>, document.getElementById('root')
 // );
+
+// Что надо написать вместо xxx, чтобы код работал?
+
+//3.
+
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+type UserType = {
+    id: number
+    name: string
+    age: number
+}
+
+function User(props: UserType) {
+    return (
+        <li>User {props.name}: {props.age} y.o.</li>
+    )
+}
+
+function UsersList() {
+    const data: Array<UserType> = [
+        {id: 1, name: "Bob", age: 25},
+        {id: 2, name: "Alex", age: 28},
+        {id: 3, name: "Ann", age: 23},
+        {id: 4, name: "John", age: 30},
+    ]
+    const [users, setUsers] = useState<Array<UserType>>(data)
+    // Пользователи старше 25 лет:
+    const olderThen25Users = users.filter(u => u.age > 25)                           // filter
+    return (
+        <main>
+            <h4>User list:</h4>
+            <ul>
+                { olderThen25Users.map(u => <User key={u.id} {...u}/>)}
+            </ul>
+        </main>
+    )
+}
+
+ReactDOM.render(
+    <UsersList/>, document.getElementById('root')
+);
 
 // Что надо написать вместо xxx, чтобы код работал?
 
