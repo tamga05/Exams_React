4d01af83a55f29a09da6d2a36b4304b406dd0641
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-07-26 01:00:51 +0300

    task solving Mon-3-6

--- src/components/Monday_3.tsx
+++ src/components/Monday_3.tsx
@@ -202,54 +202,6 @@ export default Monday_3;
 
 //5.
 
-import React, {useState} from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-
-type UserType = {
-    id: number
-    name: string
-    age: number
-}
-
-function User(props: UserType) {
-    return (
-        <li>User {props.name}: {props.age} y.o.</li>
-    )
-}
-
-function UsersList() {
-    const data: Array<UserType> = [
-        {id: 1, name: "Bob", age: 25},
-        {id: 2, name: "Alex", age: 28},
-        {id: 3, name: "Ann", age: 23},
-        {id: 4, name: "John", age: 30},
-    ]
-    const [users, setUsers] = useState<Array<UserType>>(data)
-    // Необходимо отрендерить список пользователей старше 25 лет:
-
-    const getOlderThen25Users = (u: UserType) => u.age > 25
-
-    const olderThen25Users = users.filter(getOlderThen25Users)
-
-    return (
-        <main>
-            <h4>User list:</h4>
-            <ul>
-                { olderThen25Users.map(u => <User key={u.id} {...u}/>)}
-            </ul>
-        </main>
-    )
-}
-
-ReactDOM.render(
-    <UsersList/>, document.getElementById('root')
-);
-
-// Что вернёт выражение: Array.isArray(olderThen25Users)                                    // true
-
-//6.
-
 // import React, {useState} from 'react';
 // import ReactDOM from 'react-dom';
 // import './index.css';
@@ -260,17 +212,9 @@ ReactDOM.render(
 //     age: number
 // }
 //
-// type UserPropsType = UserType & {
-//     deleteUser: (id: number) => void
-// }
-//
-// function User(props: UserPropsType) {
-//     const deleteUser = () => props.deleteUser(props.id)
+// function User(props: UserType) {
 //     return (
-//         <li>
-//             <button onClick={deleteUser}>x</button>
-//             User {props.name}: {props.age} y.o.
-//         </li>
+//         <li>User {props.name}: {props.age} y.o.</li>
 //     )
 // }
 //
@@ -282,22 +226,17 @@ ReactDOM.render(
 //         {id: 4, name: "John", age: 30},
 //     ]
 //     const [users, setUsers] = useState<Array<UserType>>(data)
+//     // Необходимо отрендерить список пользователей старше 25 лет:
 //
-//     const deleteUser = (userID: number) => {
+//     const getOlderThen25Users = (u: UserType) => u.age > 25
 //
-//         const filteredUsers = users.filter(u => u.id !== userID)
+//     const olderThen25Users = users.filter(getOlderThen25Users)
 //
-//         setUsers(xxx)                                                               // filteredUsers
-//     }
 //     return (
 //         <main>
 //             <h4>User list:</h4>
 //             <ul>
-//                 {users.map(u => <User
-//                     key={u.id}
-//                     {...u}
-//                     deleteUser={deleteUser}
-//                 />)}
+//                 { olderThen25Users.map(u => <User key={u.id} {...u}/>)}
 //             </ul>
 //         </main>
 //     )
@@ -306,6 +245,67 @@ ReactDOM.render(
 // ReactDOM.render(
 //     <UsersList/>, document.getElementById('root')
 // );
+
+// Что вернёт выражение: Array.isArray(olderThen25Users)                                    // true
+
+//6.
+
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+type UserType = {
+    id: number
+    name: string
+    age: number
+}
+
+type UserPropsType = UserType & {
+    deleteUser: (id: number) => void
+}
+
+function User(props: UserPropsType) {
+    const deleteUser = () => props.deleteUser(props.id)
+    return (
+        <li>
+            <button onClick={deleteUser}>x</button>
+            User {props.name}: {props.age} y.o.
+        </li>
+    )
+}
+
+function UsersList() {
+    const data: Array<UserType> = [
+        {id: 1, name: "Bob", age: 25},
+        {id: 2, name: "Alex", age: 28},
+        {id: 3, name: "Ann", age: 23},
+        {id: 4, name: "John", age: 30},
+    ]
+    const [users, setUsers] = useState<Array<UserType>>(data)
+
+    const deleteUser = (userID: number) => {
+
+        const filteredUsers = users.filter(u => u.id !== userID)
+
+        setUsers(filteredUsers)                                                               // filteredUsers
+    }
+    return (
+        <main>
+            <h4>User list:</h4>
+            <ul>
+                {users.map(u => <User
+                    key={u.id}
+                    {...u}
+                    deleteUser={deleteUser}
+                />)}
+            </ul>
+        </main>
+    )
+}
+
+ReactDOM.render(
+    <UsersList/>, document.getElementById('root')
+);
 
 // Что надо написать вместо xxx, чтобы код работал?
 
