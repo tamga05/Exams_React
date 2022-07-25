bedfe68123ee199c0001a2f240f94445533748f5
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-07-26 01:03:28 +0300

    task solving Mon-3-7

--- src/components/Monday_3.tsx
+++ src/components/Monday_3.tsx
@@ -250,80 +250,80 @@ export default Monday_3;
 
 //6.
 
-import React, {useState} from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-
-type UserType = {
-    id: number
-    name: string
-    age: number
-}
-
-type UserPropsType = UserType & {
-    deleteUser: (id: number) => void
-}
-
-function User(props: UserPropsType) {
-    const deleteUser = () => props.deleteUser(props.id)
-    return (
-        <li>
-            <button onClick={deleteUser}>x</button>
-            User {props.name}: {props.age} y.o.
-        </li>
-    )
-}
-
-function UsersList() {
-    const data: Array<UserType> = [
-        {id: 1, name: "Bob", age: 25},
-        {id: 2, name: "Alex", age: 28},
-        {id: 3, name: "Ann", age: 23},
-        {id: 4, name: "John", age: 30},
-    ]
-    const [users, setUsers] = useState<Array<UserType>>(data)
-
-    const deleteUser = (userID: number) => {
-
-        const filteredUsers = users.filter(u => u.id !== userID)
-
-        setUsers(filteredUsers)                                                               // filteredUsers
-    }
-    return (
-        <main>
-            <h4>User list:</h4>
-            <ul>
-                {users.map(u => <User
-                    key={u.id}
-                    {...u}
-                    deleteUser={deleteUser}
-                />)}
-            </ul>
-        </main>
-    )
-}
-
-ReactDOM.render(
-    <UsersList/>, document.getElementById('root')
-);
+// import React, {useState} from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+//
+// type UserType = {
+//     id: number
+//     name: string
+//     age: number
+// }
+//
+// type UserPropsType = UserType & {
+//     deleteUser: (id: number) => void
+// }
+//
+// function User(props: UserPropsType) {
+//     const deleteUser = () => props.deleteUser(props.id)
+//     return (
+//         <li>
+//             <button onClick={deleteUser}>x</button>
+//             User {props.name}: {props.age} y.o.
+//         </li>
+//     )
+// }
+//
+// function UsersList() {
+//     const data: Array<UserType> = [
+//         {id: 1, name: "Bob", age: 25},
+//         {id: 2, name: "Alex", age: 28},
+//         {id: 3, name: "Ann", age: 23},
+//         {id: 4, name: "John", age: 30},
+//     ]
+//     const [users, setUsers] = useState<Array<UserType>>(data)
+//
+//     const deleteUser = (userID: number) => {
+//
+//         const filteredUsers = users.filter(u => u.id !== userID)
+//
+//         setUsers(filteredUsers)                                                               // filteredUsers
+//     }
+//     return (
+//         <main>
+//             <h4>User list:</h4>
+//             <ul>
+//                 {users.map(u => <User
+//                     key={u.id}
+//                     {...u}
+//                     deleteUser={deleteUser}
+//                 />)}
+//             </ul>
+//         </main>
+//     )
+// }
+//
+// ReactDOM.render(
+//     <UsersList/>, document.getElementById('root')
+// );
 
 // Что надо написать вместо xxx, чтобы код работал?
 
 //7.
 
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-//
-//
-// function Button() {
-//     return <button onClick={xxx}>Click</button>                                       // () => alert()
-// }
-//
-//
-// ReactDOM.render(
-//     <Button/>, document.getElementById('root')
-// );
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+function Button() {
+    return <button onClick={()=>alert()}>Click</button>                                       // () => alert()
+}
+
+
+ReactDOM.render(
+    <Button/>, document.getElementById('root')
+);
 
 // Что надо написать вместо ххх,
 // что бы на странице появился пустой alert при клике по кнопке?
