56d210ec69b2ce61842c7c75c6af2294a1cb78d2
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-07-28 01:17:33 +0300

    task solving Mon-4-1

--- src/components/Monday_3.tsx
+++ src/components/Monday_3.tsx
@@ -378,57 +378,57 @@ export default Monday_3;
 
 //10.
 
-import React, {useState} from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-
-type UserType = {
-    id: number
-    name: string
-    age: number
-}
-
-type UserPropsType = UserType & {
-    deleteUser: (id: any) => void                                                                        // number
-}
-
-function User(props: UserPropsType) {
-    return (
-        <li>
-            <button onClick={()=>props.deleteUser(props.id)}>x</button>
-            User {props.name}: {props.age} y.o.
-        </li>
-    )
-}
-
-function UsersList() {
-    const data: Array<UserType> = [
-        {id: 1, name: "Bob", age: 25},
-        {id: 2, name: "Alex", age: 28},
-        {id: 3, name: "Ann", age: 23},
-        {id: 4, name: "John", age: 30},
-    ]
-    const [users, setUsers] = useState<Array<UserType>>(data)
-    const deleteUser = (userID: number) => {
-        setUsers(users.filter(u => u.id !== userID))
-    }
-    return (
-        <main>
-            <h4>Users list:</h4>
-            <ul>
-                {users.map(u => <User
-                    key={u.id}
-                    {...u}
-                    deleteUser={deleteUser}
-                />)}
-            </ul>
-        </main>
-    )
-}
-
-ReactDOM.render(
-    <UsersList/>, document.getElementById('root')
-);
+// import React, {useState} from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+//
+// type UserType = {
+//     id: number
+//     name: string
+//     age: number
+// }
+//
+// type UserPropsType = UserType & {
+//     deleteUser: (id: any) => void                                                                        // number
+// }
+//
+// function User(props: UserPropsType) {
+//     return (
+//         <li>
+//             <button onClick={()=>props.deleteUser(props.id)}>x</button>
+//             User {props.name}: {props.age} y.o.
+//         </li>
+//     )
+// }
+//
+// function UsersList() {
+//     const data: Array<UserType> = [
+//         {id: 1, name: "Bob", age: 25},
+//         {id: 2, name: "Alex", age: 28},
+//         {id: 3, name: "Ann", age: 23},
+//         {id: 4, name: "John", age: 30},
+//     ]
+//     const [users, setUsers] = useState<Array<UserType>>(data)
+//     const deleteUser = (userID: number) => {
+//         setUsers(users.filter(u => u.id !== userID))
+//     }
+//     return (
+//         <main>
+//             <h4>Users list:</h4>
+//             <ul>
+//                 {users.map(u => <User
+//                     key={u.id}
+//                     {...u}
+//                     deleteUser={deleteUser}
+//                 />)}
+//             </ul>
+//         </main>
+//     )
+// }
+//
+// ReactDOM.render(
+//     <UsersList/>, document.getElementById('root')
+// );
 
 // В типе UserPropsType у функции deleteUser в параметрах указан тип "any".
 // Какой тип было бы указать правильнее?
--- src/components/Monday_4.tsx
+++ src/components/Monday_4.tsx
@@ -16,58 +16,58 @@ export default Monday_4;
 
 //1.
 
-// import React, {useState} from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-//
-// type UserType = {
-//     id: number
-//     name: string
-//     age: number
-// }
-//
-// type UserPropsType = UserType & {
-//     deleteUser: (id: number) => void
-// }
-//
-// function User(props: UserPropsType) {
-//     return (
-//         <li>
-//             <button onClick={() => props.deleteUser(props.id)}>x</button>
-//             User {props.name}: {props.age} y.o.
-//         </li>
-//     )
-// }
-//
-// function UsersList() {
-//     const data: Array<UserType> = [
-//         {id: 1, name: "Bob", age: 25},
-//         {id: 2, name: "Alex", age: 28},
-//         {id: 3, name: "Ann", age: 23},
-//         {id: 4, name: "John", age: 30},
-//     ]
-//     const [users, setUsers] = useState<Array<UserType>>(data)
-//     const deleteUser = (userID: number) => {
-//         const filteredUsers = users.filter(u => u.id !== userID)
-//         setUsers(filteredUsers)
-//     }
-//     return (
-//         <main>
-//             <h4>User list:</h4>
-//             <ul>
-//                 {users.map(u => <User
-//                     key={u.id}
-//                     {...u}
-//                     deleteUser={deleteUser}
-//                 />)}
-//             </ul>
-//         </main>
-//     )
-// }
-//
-// ReactDOM.render(
-//     <UsersList/>, document.getElementById('root')
-// );
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+type UserType = {
+    id: number
+    name: string
+    age: number
+}
+
+type UserPropsType = UserType & {
+    deleteUser: (id: number) => void
+}
+
+function User(props: UserPropsType) {
+    return (
+        <li>
+            <button onClick={() => props.deleteUser(props.id)}>x</button>
+            User {props.name}: {props.age} y.o.
+        </li>
+    )
+}
+
+function UsersList() {
+    const data: Array<UserType> = [
+        {id: 1, name: "Bob", age: 25},
+        {id: 2, name: "Alex", age: 28},
+        {id: 3, name: "Ann", age: 23},
+        {id: 4, name: "John", age: 30},
+    ]
+    const [users, setUsers] = useState<Array<UserType>>(data)
+    const deleteUser = (userID: number) => {
+        const filteredUsers = users.filter(u => u.id !== userID)
+        setUsers(filteredUsers)
+    }
+    return (
+        <main>
+            <h4>User list:</h4>
+            <ul>
+                {users.map(u => <User
+                    key={u.id}
+                    {...u}
+                    deleteUser={deleteUser}
+                />)}
+            </ul>
+        </main>
+    )
+}
+
+ReactDOM.render(
+    <UsersList/>, document.getElementById('root')
+);
 
 // Что надо написать вместо xxx, чтобы код работал?
 
