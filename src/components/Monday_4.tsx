62fc4b3181f4e8fd87414aa5a47843c5bb77cfcb
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-07-29 00:05:26 +0300

    task solving Mon-4-3

--- src/components/Monday_4.tsx
+++ src/components/Monday_4.tsx
@@ -73,91 +73,91 @@ export default Monday_4;
 
 //2.
 
-import React, {useState} from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-
-type UserType = {
-    id: number
-    name: string
-    age: number
-}
-
-type UserPropsType = UserType & {
-    deleteUser: (id: number) => void
-}
-
-function User(props: UserPropsType) {
-    const deleteUser = () => props.deleteUser(props.id)
-    return (
-        <li>
-            <button onClick={deleteUser}>Delete</button>
-            User {props.name}: {props.age} y.o.
-        </li>
-    )
-}
-
-function UsersList() {
-    const data: Array<UserType> = [
-        {id: 1, name: "Bob", age: 25},
-        {id: 2, name: "Alex", age: 28},
-        {id: 3, name: "Ann", age: 23},
-        {id: 4, name: "John", age: 30},
-    ]
-    const [users, setUsers] = useState<Array<UserType>>(data)
-    const deleteUser = (userID: number) => {
-        const updatedUsers = users.filter(u => u.id !== userID)
-        setUsers(updatedUsers)
-    }
-    return (
-        <main>
-            <h4>User list:</h4>
-            <ul>
-                {users.map(u => <User
-                    key={u.id}
-                    {...u}
-                    deleteUser={deleteUser}
-                />)}
-            </ul>
-        </main>
-    )
-}
-
-ReactDOM.render(
-    <UsersList/>, document.getElementById('root')
-);
+// import React, {useState} from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+//
+// type UserType = {
+//     id: number
+//     name: string
+//     age: number
+// }
+//
+// type UserPropsType = UserType & {
+//     deleteUser: (id: number) => void
+// }
+//
+// function User(props: UserPropsType) {
+//     const deleteUser = () => props.deleteUser(props.id)
+//     return (
+//         <li>
+//             <button onClick={deleteUser}>Delete</button>
+//             User {props.name}: {props.age} y.o.
+//         </li>
+//     )
+// }
+//
+// function UsersList() {
+//     const data: Array<UserType> = [
+//         {id: 1, name: "Bob", age: 25},
+//         {id: 2, name: "Alex", age: 28},
+//         {id: 3, name: "Ann", age: 23},
+//         {id: 4, name: "John", age: 30},
+//     ]
+//     const [users, setUsers] = useState<Array<UserType>>(data)
+//     const deleteUser = (userID: number) => {
+//         const updatedUsers = users.filter(u => u.id !== userID)
+//         setUsers(updatedUsers)
+//     }
+//     return (
+//         <main>
+//             <h4>User list:</h4>
+//             <ul>
+//                 {users.map(u => <User
+//                     key={u.id}
+//                     {...u}
+//                     deleteUser={deleteUser}
+//                 />)}
+//             </ul>
+//         </main>
+//     )
+// }
+//
+// ReactDOM.render(
+//     <UsersList/>, document.getElementById('root')
+// );
 
 // Что надо написать вместо xxx, чтобы код работал?
 
 //3.
 
-// import React, {useState, MouseEvent} from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-//
-// function Post() {
-//     const onClickHandler = (e: MouseEvent<HTMLAnchorElement>) => {
-//         e.preventDefault()
-//         alert('Летим-бомбим!!!')
-//     }
-//     return (
-//         <div>
-//             <article>
-//                 <h4>Как дела, братан?</h4>
-//                 <p>
-//                     Вижу, что неплохо. Давай, трудись )))
-//                     Google ждёт тебя в цифровом рабстве!
-//                     Cтавь лайк и полетели!!!
-//                 </p>
-//                 <a href={'https://www.youtube.com/'} onClick={onClickHandler}>В этом месте подробнее...</a>
-//             </article>
-//         </div>
-//     )
-// }
-//
-// ReactDOM.render(
-//     <Post/>, document.getElementById('root')
-// );
+import React, {MouseEvent} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function Post() {
+    const onClickHandler = (e: MouseEvent<HTMLAnchorElement>) => {
+        e.preventDefault()
+        alert('Летим-бомбим!!!')
+    }
+    return (
+        <div>
+            <article>
+                <h4>Как дела, братан?</h4>
+                <p>
+                    Вижу, что неплохо. Давай, трудись )))
+                    Google ждёт тебя в цифровом рабстве!
+                    Cтавь лайк и полетели!!!
+                </p>
+                <a href={'https://www.youtube.com/'} onClick={onClickHandler}>В этом месте подробнее...</a>
+            </article>
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <Post/>, document.getElementById('root')
+);
 
 // Что надо написать вместо ххх, чтобы Вас не направило на страницу Youtube
 // при клике по ссылке?
