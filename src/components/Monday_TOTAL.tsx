72698df1d62df88867583ea1790659a43cefdf69
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-08-07 10:09:13 +0300

    task solving Mon-TOTAL-1

--- src/components/Monday_TOTAL.tsx
+++ src/components/Monday_TOTAL.tsx
@@ -17,16 +17,16 @@ export default MondayTotal;
 
 //1.
 
-// type UserPropsType = {
-//     name: string
-//     description: string
-// }
-// export const User: React.FC<UserPropsType> = (props) => {
-//     return <div>
-//         <h1>Имя: {props.name}</h1>
-//         //<div>Описание: {props.description}</div>
-//     </div>
-// }
+type UserPropsType = {
+    name: string
+    description: string
+}
+export const User: React.FC<UserPropsType> = (props) => {
+    return <div>
+        <h1>Имя: {props.name}</h1>
+        //<div>Описание: {props.description}</div>
+    </div>
+}
 
 //Что нужно написать вместо ххх, что бы код работал?
 
