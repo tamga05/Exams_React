0c316f9c961170ae999a93b11d162c6355e25147
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-09-01 11:25:42 +0300

    task solving Mon-TOTAL-12

--- src/components/Monday_TOTAL.tsx
+++ src/components/Monday_TOTAL.tsx
@@ -322,70 +322,70 @@ export default MondayTotal;
 
 //11.
 
-import React, {useState, MouseEvent, ChangeEvent} from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-
-function User() {
-    const [userName, setUserName] = useState<string>("")
-    return (
-        <div>
-            <p>{userName}</p>
-            <input
-                value={userName}
-                onChange={(e) => setUserName(e.currentTarget.value)}
-            />
-        </div>
-    )
-}
-
-ReactDOM.render(
-    <User/>, document.getElementById('root')
-);
-
-// Что надо написать вместо ххх, чтобы инпут был контролируемым?
-
-//12.
-
 // import React, {useState, MouseEvent, ChangeEvent} from 'react';
 // import ReactDOM from 'react-dom';
 // import './index.css';
 //
-// function Notes() {
-//     const [newNote, setNewNote] = useState<string>("")
-//     const [notes, setNotes] = useState<Array<string>>([])
-//
-//     const onChangeHandler = (e: ChangeEvent<HTMLTextAreaElement> )=>
-//         setNewNote(e.currentTarget.value)
-//
-//     const addNote = () => {
-//         setNotes([newNote, ...notes])
-//         setNewNote("")
-//     }
-//
+// function User() {
+//     const [userName, setUserName] = useState<string>("")
 //     return (
 //         <div>
-//             <textarea
-//                 value={newNote}
-//                 onChange={onChangeHandler}
-//                 onBlur={addNote}
+//             <p>{userName}</p>
+//             <input
+//                 value={userName}
+//                 onChange={(e) => setUserName(e.currentTarget.value)}
 //             />
-//             <div>
-//                 <button
-//                     onClick={()=>setNotes([])}
-//                 >Clear notes list</button>
-//             </div>
-//             <h4>Notes:</h4>
-//             <div>
-//                 {notes.map(n => <p>{n}</p>)}
-//             </div>
 //         </div>
 //     )
 // }
 //
 // ReactDOM.render(
-//     <Notes/>, document.getElementById('root')
+//     <User/>, document.getElementById('root')
 // );
+
+// Что надо написать вместо ххх, чтобы инпут был контролируемым?
+
+//12.
+
+import React, {useState, MouseEvent, ChangeEvent} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function Notes() {
+    const [newNote, setNewNote] = useState<string>("")
+    const [notes, setNotes] = useState<Array<string>>([])
+
+    const onChangeHandler = (e: ChangeEvent<HTMLTextAreaElement> )=>
+        setNewNote(e.currentTarget.value)
+
+    const addNote = () => {
+        setNotes([newNote, ...notes])
+        setNewNote("")
+    }
+
+    return (
+        <div>
+            <textarea
+                value={newNote}
+                onChange={onChangeHandler}
+                onBlur={addNote}
+            />
+            <div>
+                <button
+                    onClick={()=>setNotes([])}
+                >Clear notes list</button>
+            </div>
+            <h4>Notes:</h4>
+            <div>
+                {notes.map(n => <p>{n}</p>)}
+            </div>
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <Notes/>, document.getElementById('root')
+);
 
 // Что надо написать вместо ххх,
 // чтобы при клике список заметок очищался?
