215720c2fe21f11334a1d78816a48172594f5a8d
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-09-02 08:50:53 +0300

    task solving Tue-1-1

--- src/components/Tuesday_1.tsx
+++ src/components/Tuesday_1.tsx
@@ -17,28 +17,28 @@ export default Tuesday_1;
 
 //1.
 
-// import React, {useState, MouseEvent} from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-//
-//
-// function ColorButton() {
-//     const [isColored, setIsColored] = useState<boolean>(false)
-//
-//     return (
-//         <button
-//             style={{backgroundColor: `${isColored ? 'red' : ''}`}}                       // isColored
-//             onClick={() => setIsColored(true)}
-//         >
-//             Меняю цвет по клику
-//         </button>
-//     )
-// }
-//
-//
-// ReactDOM.render(
-//     <ColorButton/>, document.getElementById('root')
-// );
+import React, {useState, MouseEvent} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+function ColorButton() {
+    const [isColored, setIsColored] = useState<boolean>(false)
+
+    return (
+        <button
+            style={{backgroundColor: `${isColored ? 'red' : ''}`}}                       // isColored
+            onClick={() => setIsColored(true)}
+        >
+            Меняю цвет по клику
+        </button>
+    )
+}
+
+
+ReactDOM.render(
+    <ColorButton/>, document.getElementById('root')
+);
 
 // Что надо написать вместо XXX, чтобы при клике кнопка становилась красной?
 
