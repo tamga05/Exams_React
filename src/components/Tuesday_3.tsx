ca0f93c7136f1ea58647e9e303dfd71f36befc6b
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-09-22 16:35:02 +0300

    task solving Tue-3-3

--- src/components/Tuesday_3.tsx
+++ src/components/Tuesday_3.tsx
@@ -60,49 +60,6 @@ export default Tuesday_3;
 
 // 2.
 
-import React, {useState} from "react";
-
-type UserType = {
-    id: number
-    name: string
-    status: "online" | "offline"
-}
-
-type AddressType = {
-    country: string
-    city: string
-    email: string
-}
-
-type AdressesType = {
-    [userID: string]: AddressType
-}
-
-
-const users: Array<UserType> = [
-    {id: 1, name: "Bob", status: "online"},
-    {id: 2, name: "Alex", status: "offline"},
-    {id: 3, name: "Ann", status: "offline"},
-]
-
-export const addresses: AdressesType = {
-    1: {country: "Russia", city: "Moscow", email: "hey@email.com"},
-    2: {country: "Ukraine", city: "Kiev", email: "yo@send.ua"},
-    3: {country: "Belarus", city: "Minsk", email: "wow@gogo.ru"},
-
-}
-
-export const changeUserStatus = (userID: number, status: string) => {
-    return users.map(u => u.id === userID ? {...u, status} : u)                                                      // map
-}
-
-// Дан список пользователей и структура, хранящая адреса пользователей.
-// Так же дана функция changeUserStatus, которая меняет статус пользователя.
-// Что надо написать вместо ххх, чтобы функция работала корректно?
-
-
-// 3.
-
 // import React, {useState} from "react";
 //
 // type UserType = {
@@ -121,24 +78,67 @@ export const changeUserStatus = (userID: number, status: string) => {
 //     [userID: string]: AddressType
 // }
 //
+//
 // const users: Array<UserType> = [
 //     {id: 1, name: "Bob", status: "online"},
 //     {id: 2, name: "Alex", status: "offline"},
 //     {id: 3, name: "Ann", status: "offline"},
 // ]
 //
-// const addresses: AdressesType = {
+// export const addresses: AdressesType = {
 //     1: {country: "Russia", city: "Moscow", email: "hey@email.com"},
 //     2: {country: "Ukraine", city: "Kiev", email: "yo@send.ua"},
 //     3: {country: "Belarus", city: "Minsk", email: "wow@gogo.ru"},
 //
 // }
 //
-// const updateUserAddress = (userID: number, key: string, newValue: string) => {
-//     return {...addresses,
-//         [userID]: {...addresses[userID], [key]: newValue}                                                              // userID
-//     }
+// export const changeUserStatus = (userID: number, status: string) => {
+//     return users.map(u => u.id === userID ? {...u, status} : u)                                                      // map
 // }
+
+// Дан список пользователей и структура, хранящая адреса пользователей.
+// Так же дана функция changeUserStatus, которая меняет статус пользователя.
+// Что надо написать вместо ххх, чтобы функция работала корректно?
+
+
+// 3.
+
+import React, {useState} from "react";
+
+type UserType = {
+    id: number
+    name: string
+    status: "online" | "offline"
+}
+
+type AddressType = {
+    country: string
+    city: string
+    email: string
+}
+
+type AdressesType = {
+    [userID: string]: AddressType
+}
+
+const users: Array<UserType> = [
+    {id: 1, name: "Bob", status: "online"},
+    {id: 2, name: "Alex", status: "offline"},
+    {id: 3, name: "Ann", status: "offline"},
+]
+
+const addresses: AdressesType = {
+    1: {country: "Russia", city: "Moscow", email: "hey@email.com"},
+    2: {country: "Ukraine", city: "Kiev", email: "yo@send.ua"},
+    3: {country: "Belarus", city: "Minsk", email: "wow@gogo.ru"},
+
+}
+
+const updateUserAddress = (userID: number, key: string, newValue: string) => {
+    return {...addresses,
+        [userID]: {...addresses[userID], [key]: newValue}                                                              // userID
+    }
+}
 
 // Дан список пользователей и структура, хранящая адреса пользователей.
 // Так же дана функция updateUserAddress,
